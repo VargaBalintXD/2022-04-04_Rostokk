b34e4197e930a135806859d78c7d596569eabf74
VargaBalintXD <vargab.12d@acsjszki.hu>
2022-04-04 12:55:39 +0200

    Add project files.

---
+++ 2022-04-04_Rostokk/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _2022_04_04_Rostokk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<RostokAdatok> rostokAdatoks = new List<RostokAdatok>();
+            foreach (var sor in File.ReadAllLines("rostok.txt").Skip(1)) 
+
+            {
+                rostokAdatoks.Add(new RostokAdatok(sor));
+            }
+            Console.WriteLine($"3.feladat:{rostokAdatoks.Count}");
+
+            Console.ReadKey();
+        }
+
+    }
+
+}
---
+++ 2022-04-04_Rostokk/RostokAdatok.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2022_04_04_Rostokk
+{
+    class RostokAdatok
+    {
+        public string Megnevezés     { get; set; }
+        public string Kategória { get; set; }
+        public string Egység { get; set; }
+        public double Rost { get; set; }
+    
+        public RostokAdatok (string sor)
+        {
+            string[] t = sor.Split(';');
+            Megnevezés = t[0];
+            Kategória = t[1];
+            Egység = t[2];
+            Rost = double.Parse(t[3]);
+
+
+        }
+    
+    
+    
+    
+    }
+    
+}
